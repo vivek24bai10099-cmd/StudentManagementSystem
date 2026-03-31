f08134614bf507a134d3f2697e60728f8eafff9a
vivek24bai10099-cmd <vivek.24bai10099@vitbhopal.ac.in>
2026-03-31 19:40:35 +0530

    Create FileHandler.java

---
+++ src/FileHandler.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class FileHandler {
+
+    public static void saveToFile(ArrayList<Student> students) {
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter("students.txt"));
+            for (Student s : students) {
+                pw.println(s.toFileString());
+            }
+            pw.close();
+            System.out.println("Data saved to file.");
+        } catch (IOException e) {
+            System.out.println("Error saving file.");
+        }
+    }
+}
