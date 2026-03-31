0ec55c10d5ba0381bb2bf3e7ee6882f555519743
vivek24bai10099-cmd <vivek.24bai10099@vitbhopal.ac.in>
2026-03-31 19:38:32 +0530

    Create Student.java

---
+++ src/Student.java
@@ -0,0 +1,26 @@
+public class Student {
+    private int id;
+    private String name;
+    private String course;
+    private double marks;
+
+    public Student(int id, String name, String course, double marks) {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+        this.marks = marks;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String toFileString() {
+        return id + "," + name + "," + course + "," + marks;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " Name: " + name + " Course: " + course + " Marks: " + marks;
+    }
+}
